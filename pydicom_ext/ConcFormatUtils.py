133542d5ea17a4d1129e3c8802a4c1af046723e4
Tetsuya Shinaji <shina.synergy@gmail.com>
2017-12-27 08:25:09 +0100

    add save function for conc format

--- pydicom_ext/ConcFormatUtils.py
+++ pydicom_ext/ConcFormatUtils.py
@@ -10,6 +10,7 @@
 
 """
 from collections import OrderedDict
+import re
 
 
 class AttrDict(OrderedDict):
@@ -100,6 +101,26 @@ class ConcFormatHeaderManager:
                         tmp_info[data[0]] = tmp
             frame_info.append(tmp_info)
         return basic_info, frame_info
+
+    def save_hdr(self, filename):
+        new_hdr = ""
+        for key in self.basic_info.keys():
+            if len(re.findall("(comment_)", key)) > 0:
+                new_hdr += f"{self.basic_info[key]}\n"
+            else:
+                new_hdr += f"{key} {self.basic_info[key]}\n".replace("[", ' ').replace("]", ' ').replace(",", '')
+        for i in range(len(self.frame_info)):
+            for key in self.frame_info[i].keys():
+                if len(re.findall("(comment_)", key)) > 0:
+                    new_hdr += f"{self.frame_info[i][key]}\n"
+                elif len(re.findall("(singles_)", key)) > 0:
+                    new_hdr += f"{key}{self.frame_info[i][key]}\n".replace("_", " ").replace("[", ' ').replace("]", ' ').replace(",", '')
+                else:
+                    new_hdr += f"{key} {self.frame_info[i][key]}\n".replace("[", ' ').replace("]", ' ').replace(",", '')
+        new_hdr = new_hdr
+        with open(filename, "wb") as f:
+            f.write(new_hdr.replace(('\r'), '').encode('utf-8'))
+
 
 if __name__ == '__main__':
     pass
